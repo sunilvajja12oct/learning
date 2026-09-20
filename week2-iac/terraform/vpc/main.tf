292c213f5995654034afb4ebaa244f7445b76f85
sunil <sunilvajja12oct@gmail.com>
2026-09-20 11:45:20 -0500

    Clean up VPC lab: commit lock file, ignore generated graph, remove typo'd duplicate output file

--- week2-iac/terraform/vpc/main.tf
+++ week2-iac/terraform/vpc/main.tf
@@ -1,1 +1,20 @@
-provider 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "prep-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prep-public-subnet"
+  }
+}
